c928cad7f06463c38b796aad810f75729370036f
bhaveshgoswami02 <bhaveshgoswami2381998@gmail.com>
2020-06-16 13:04:08 +0530

    first Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { AllProductsComponent } from './Components/all-products/all-products.component';
+import { ProductDetailsComponent } from './Components/product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AllProductsComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
